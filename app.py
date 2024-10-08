4baf3dd9e283fdd4a153bf3b573bec12b875670b
Michal Urbanczyk <michaurbanczyk@gmail.com>
2024-10-08 20:18:15 +0200

    initial commit

---
+++ app.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get('/')
+def hello_world():
+    return "Hello,World"
+
+
+if __name__ == '__main__':
+    import uvicorn
+    uvicorn.run(app)
