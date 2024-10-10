6783af7c2c586baa679aac27f74b94b3ea7a9f89
Michal Urbanczyk <michaurbanczyk@gmail.com>
2024-10-10 21:45:28 +0200

    request body added

--- app.py
+++ app.py
@@ -16,25 +16,29 @@ data = {
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:63343"],  # Or ["*"] to allow all origins
+    allow_origins=["https://marikate-dev.vercel.app", "http://localhost:63342"],  # Or ["*"] to allow all origins
     allow_credentials=True,
     allow_methods=["*"],  # Allow all methods (GET, POST, etc.)
     allow_headers=["*"],  # Allow all headers
 )
 
+
 @app.post('/create-order')
-def create_order(request: Request):
+async def create_order(request: Request):
     get_token = requests.post(URL_TOKEN, data=data)
     access_token = get_token.json()["access_token"]
+
+    request_body = await request.json()
+    amount = request_body.get("amount")
     client_id = request.client.host
 
     create_order_body = {
-        "continueUrl": "https://marikate-fund-modern-dev.vercel.app/success",
+        "continueUrl": "https://marikate-dev.vercel.app/success.html",
         "customerIp": client_id,
         "merchantPosId": "485425",
-        "description": "RTV market",
+        "description": "Donation",
         "currencyCode": "PLN",
-        "totalAmount": "21000"
+        "totalAmount": str(int(amount)*100)
     }
 
     headers = {
